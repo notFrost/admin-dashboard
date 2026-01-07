76dd8888fa6996163dd2fe894b934092efba645a
Diego <diegopj97@gmail.com>
2026-01-07 00:47:26 -0500

    chore: add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+
 import "./styles/globals.css";
 import { router } from "./app/routes/routes";
+import { queryClient } from "./app/lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
